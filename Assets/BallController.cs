a7a5c968ebecb61922c632ddce3c25c6a2bd55b5
roku113 <83396955+roku113@users.noreply.github.com>
2021-09-11 13:43:13 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private float visiblePosZ = -6.5f;
+
+    private GameObject gameoverText;
+    private GameObject scoreCounter;
+
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.scoreCounter = GameObject.Find("Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+        this.scoreCounter.GetComponent<Text>().text = score.ToString();
+    
+    }
+
+    void OnCollisionEnter(Collision item)
+    {
+        if (item.gameObject.tag == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if (item.gameObject.tag == "LargeStarTag")
+        {
+            this.score += 20;
+        }
+        else if (item.gameObject.tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.score += 30;
+        }
+        
+    }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    private HingeJoint myHingeJoint;
+
+    private float defaultAngle = 20;
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        if(Input.GetKeyDown(KeyCode.D) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            SetAngle(this.defaultAngle);
+        }
+    }
+
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
